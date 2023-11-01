5316acd18693c0538150689099a0585282b5e334
Minh-creator <nguyenvanminh27062004@gmail.com>
2023-11-01 16:53:25 +0700

    dcmm

---
+++ DuAn1_PRO1041/src/duan1_pro1041/DuAn1_PRO1041.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package duan1_pro1041;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class DuAn1_PRO1041 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Xin chào");
+    }
+    
+}
